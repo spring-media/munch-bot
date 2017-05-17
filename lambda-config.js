2bb6af79114df61538d238da7a82aa650f783d40
cgohlke <christian.gohlke@weltn24.de>
2017-05-17 11:47:03 +0200

    swtich to node 6 runtime

--- lambda-config.js
+++ lambda-config.js
@@ -6,5 +6,5 @@ module.exports = {
     timeout: 10,
     memorySize: 128,
     publish: true, // default: false,
-    runtime: 'nodejs4.3', // default: 'nodejs',
+    runtime: 'nodejs6.10', // default: 'nodejs',
 };
